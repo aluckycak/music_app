140c7f2db47382ae1bd64c917d0631662aa5e798
aluckycak <1173659275@qq.com>
2022-05-18 15:11:46 +0800

    2 commit

--- music_vue/src/router/index.js
+++ music_vue/src/router/index.js
@@ -1,25 +1,42 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home'
+import layout from "../views/layout.vue"
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    Path: "/",
+    name: "layout",
+    component: layout,
+    children: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home
+      },
+      {
+        path: '/topic',
+        name: 'Topic',
+        component: () => import("../views/topic.vue")
+      },
+      {
+        path: '/mine',
+        name: 'Mine',
+        component: () => import("../views/mine.vue")
+      },
+      {
+        path: '/search',
+        name: 'Search',
+        component: () => import("../views/search.vue")
+      },
+    ]
   }
+
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  linkActiveClass: "active"
 })
 
 export default router
---
+++ music_vue/src/views/layout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Nav />
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import Nav from "../components/nav.vue"
+export default {
+    components: {
+        Nav
+    }
+}
+</script>
+
+<style>
+</style>
